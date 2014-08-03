d35871addf79cccdd03ff1891a7132299bd040b6
Jason Mooberry <jasonmoo@me.com>
2014-08-02 23:59:55 -0400

    Cache and serve strategy for quicker returns

--- api.go
+++ api.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 )
 
@@ -16,8 +19,14 @@ type (
 		Status     string   `json:"status"`
 		Errors     []string `json:"errors"`
 		NumResults int      `json:"num_results"`
-		Results    []Event  `json:"results"`
+		Results    []*Event `json:"results"`
 		Copyright  string   `json:"copyright"`
+	}
+
+	EventCache struct {
+		mu        sync.RWMutex
+		Copyright string
+		Events    []*Event
 	}
 
 	Event struct {
@@ -55,10 +64,16 @@ type (
 
 		HTML   string `json:"html"`
 		MapURL string `json:"map_url"`
+
+		Lat float64 `json:"lat"`
+		Lng float64 `json:"lng"`
 	}
 )
 
-const Endpoint = "http://api.nytimes.com/svc/events/v2/listings.json"
+const (
+	Endpoint = "http://api.nytimes.com/svc/events/v2/listings.json"
+	MaxRange = (1 << 32) - 1
+)
 
 var (
 	DayTranslations = map[string]string{
@@ -73,14 +88,129 @@ var (
 	ApiKey = os.Getenv("NYT_API_KEY")
 )
 
-func Fetch(lat, lng string) (*Response, error, time.Duration) {
+func (ec *EventCache) GetResponse(swlat, swlng, nelat, nelng float64) *Response {
 
+	ec.mu.RLock()
+	defer ec.mu.RUnlock()
+
+	r := &Response{
+		Copyright: ec.Copyright,
+		Results:   make([]*Event, 0),
+	}
+
+	for _, event := range ec.Events {
+		if event.Lat > swlat && event.Lat < nelat && event.Lng > swlng && event.Lng < nelng {
+			r.Results = append(r.Results, event)
+		}
+	}
+
+	r.NumResults = len(r.Results)
+
+	return r
+
+}
+
+func LoadCache(cachefile string) (*EventCache, error) {
+
+	file, err := os.Open(cachefile)
+	if err != nil {
+		return &EventCache{Events: make([]*Event, 0)}, nil
+	}
+	defer file.Close()
+
+	ec := &EventCache{}
+	err = json.NewDecoder(file).Decode(ec)
+	if err != nil {
+		return nil, err
+	}
+
+	return ec, nil
+
+}
+
+func (ec *EventCache) RefreshData() error {
+
+	events := make([]*Event, 0)
+
+	// initial search with values and num count
+	r, err, _ := fetch(0)
+	if err != nil {
+		return err
+	}
+
+	for _, event := range r.Results {
+		events = append(events, event)
+	}
+
+	for offset := 0; offset < r.NumResults; offset += 100 {
+		result, err, _ := fetch(offset)
+		if err != nil {
+			return err
+		}
+		for _, event := range result.Results {
+			events = append(events, event)
+		}
+		time.Sleep(time.Second) // rate limit ouseves
+	}
+
+	for i, _ := range events {
+
+		// convert lat,lng for easy comparison
+		events[i].Lat, _ = strconv.ParseFloat(events[i].GeocodeLatitude, 64)
+		events[i].Lng, _ = strconv.ParseFloat(events[i].GeocodeLongitude, 64)
+
+		// cleanup day names
+		for j, day := range events[i].RecurDays {
+			events[i].RecurDays[j] = DayTranslations[day]
+		}
+
+		address := url.QueryEscape(events[i].StreetAddress + ", " + events[i].Borough + " " + events[i].State)
+		events[i].MapURL = fmt.Sprintf("https://maps.google.com/maps?saddr=Current+Location&daddr=%s", address)
+
+		// populate html
+		buf := new(bytes.Buffer)
+		err = EventTemplate.Execute(buf, events[i])
+		if err != nil {
+			events[i].HTML = err.Error()
+		} else {
+			events[i].HTML = buf.String()
+		}
+
+	}
+
+	ec.mu.Lock()
+	ec.Copyright = r.Copyright
+	ec.Events = events
+	ec.mu.Unlock()
+
+	return nil
+
+}
+
+func (ec *EventCache) WriteCache(cachefile string) error {
+
+	ec.mu.RLock()
+	defer ec.mu.RUnlock()
+
+	data, err := json.Marshal(ec)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(cachefile, data, 0666)
+
+}
+
+func fetch(offset int) (*Response, error, time.Duration) {
+	// central park:
+	// 40.767927, -73.980047
 	search := url.Values{
 		"api-key": {ApiKey},
-		"ll":      {lat + "," + lng},
-		"radius":  {"1000"}, // meters
-		"sort":    {"dist asc"},
+		"ll":      {"40.767927,-73.980047"},
+		"radius":  {strconv.Itoa(MaxRange)}, // meters
+		"sort":    {"geocode_latitude asc"},
 		"limit":   {"100"},
+		"offset":  {strconv.Itoa(offset)},
 	}
 
 	apiurl := Endpoint + "?" + search.Encode()
@@ -109,26 +239,6 @@ func Fetch(lat, lng string) (*Response, error, time.Duration) {
 		return nil, err, total_time
 	}
 
-	for i, event := range r.Results {
-
-		// cleanup day names
-		for j, day := range r.Results[i].RecurDays {
-			r.Results[i].RecurDays[j] = DayTranslations[day]
-		}
-
-		address := url.QueryEscape(event.StreetAddress + ", " + event.Borough + " " + event.State)
-		r.Results[i].MapURL = fmt.Sprintf("https://maps.google.com/maps?saddr=Current+Location&daddr=%s", address)
-
-		// populate html
-		buf := new(bytes.Buffer)
-		err := EventTemplate.Execute(buf, r.Results[i])
-		if err != nil {
-			r.Results[i].HTML = err.Error()
-		} else {
-			r.Results[i].HTML = buf.String()
-		}
-
-	}
-
 	return r, nil, total_time
+
 }
--- main.go
+++ main.go
@@ -7,14 +7,20 @@ import (
 	"log"
 	"net/http"
 	"runtime"
+	"strconv"
 	"strings"
+	"time"
 )
 
 var (
-	host   = flag.String("host", ":8080", "host name to listen on")
-	webdir = flag.String("webdir", "./web", "source of static files")
+	host      = flag.String("host", ":8080", "host name to listen on")
+	webdir    = flag.String("webdir", "./web", "source of static files")
+	cachefile = flag.String("cachefile", "./events.json", "cache of current dataset")
 
 	dev = flag.Bool("dev", false, "output dev signals")
+
+	ec  *EventCache
+	err error
 )
 
 func init() {
@@ -27,6 +33,25 @@ func main() {
 
 	flag.Parse()
 
+	ec, err = LoadCache(*cachefile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			err := ec.RefreshData()
+			if err != nil {
+				log.Println(err)
+				// retry every minute until we're back online
+				time.Sleep(time.Minute)
+				continue
+			}
+			ec.WriteCache(*cachefile)
+			time.Sleep(time.Hour)
+		}
+	}()
+
 	http.Handle("/", NewGzipFileHandler(*webdir, []string{}))
 	http.Handle("/search", NewGzipHandler(EventSearchHandler))
 	log.Fatal(http.ListenAndServe(*host, nil))
@@ -38,23 +63,26 @@ func EventSearchHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
 
 	var (
-		lat = strings.TrimSpace(req.FormValue("lat"))
-		lng = strings.TrimSpace(req.FormValue("lng"))
+		swlat_string = strings.TrimSpace(req.FormValue("swlat"))
+		swlng_string = strings.TrimSpace(req.FormValue("swlng"))
+		nelat_string = strings.TrimSpace(req.FormValue("nelat"))
+		nelng_string = strings.TrimSpace(req.FormValue("nelng"))
 	)
 
-	if len(lat) == 0 || len(lng) == 0 {
-		http.Error(w, "lat and lng required", http.StatusBadRequest)
+	swlat, _ := strconv.ParseFloat(swlat_string, 64)
+	swlng, _ := strconv.ParseFloat(swlng_string, 64)
+	nelat, _ := strconv.ParseFloat(nelat_string, 64)
+	nelng, _ := strconv.ParseFloat(nelng_string, 64)
+
+	if swlat == 0 || swlng == 0 || nelat == 0 || nelng == 0 {
+		http.Error(w, "swlat, swlng, nelat and nelng required", http.StatusBadRequest)
 		return
 	}
 
-	r, err, total_time := Fetch(lat, lng)
+	err := json.NewEncoder(w).Encode(ec.GetResponse(swlat, swlng, nelat, nelng))
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadGateway)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	w.Header().Set("X-NYTAPI-Response-Time", total_time.String())
-
-	json.NewEncoder(w).Encode(r)
 
 }
